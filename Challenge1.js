db141af31ae0edc396ba3ab032ff9e64ef9e6650
Victoria0487 <doubles1625@gmail.com>
2022-12-15 09:25:31 +0100

    initial commit

---
+++ Challenge1.js
@@ -0,0 +1,12 @@
+// Calculate the sum of numbers within an array
+
+function sumArray (arr) {
+    let total = 0;
+
+    for (i = 0; i < arr.length; i++) {
+        total += arr[i];
+    }
+    return total;
+}
+
+console.log(sumArray([2,3,1]))
